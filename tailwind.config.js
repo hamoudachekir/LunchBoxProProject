82447b9b6ad2c9dd8255d8874f5e29386fdbbacb
hamoudachekir <hamoudachkir@yahoo.fr>
2025-03-06 11:39:23 +0100

    Dernière mise à jour avec les images et les corrections de styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.html"],
+  content: ["./public/**/*.html", "./src/**/*.html"],
   theme: {
     extend: {
       colors: {
